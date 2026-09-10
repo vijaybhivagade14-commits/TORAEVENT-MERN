dac5d5a5da20c794be204f7f89bd3516e6d112a9
Vijay_dev_2026 <vijaybhivagade14@gmail.com>
2026-09-10 12:34:30 +0530

    Updated the token syntax

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -7,12 +7,11 @@ const api = axios.create({
     },
 });
 
-api.interceptors.request.use((config) => 
-{
+api.interceptors.request.use((config) => {
     const token = localStorage.getItem('token');
 
     if (token) {
-        config.headers.Authorization = `Bearer $(token)`;
+        config.headers.Authorization = `Bearer ${token}`;
 
     }
     return config;
